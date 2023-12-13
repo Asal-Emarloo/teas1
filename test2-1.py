b3592c35dafd1527e351195ab016af8ca6ca093d
Asal-Emarloo <153554023+Asal-Emarloo@users.noreply.github.com>
2023-12-13 22:41:00 +0330

    Add files via upload

---
+++ test2-1.py
@@ -0,0 +1,27 @@
+A=int(input("Enter number= "))
+B=int(input("Enter another number="))
+import math
+print("radikal A:")
+print (math.sqrt(A))
+print("radikal B:")
+print (math.sqrt(B))
+
+print("sin A:")
+print (math.sin(A))
+print("sin B:")
+print (math.sin(B))
+
+print("cos A:")
+print (math.cos(A))
+print("cos B:")
+print (math.cos(B))
+
+print("tan A:")
+print (math.tan(A))
+print("tan B:")
+print (math.tan(B))
+
+print("A+B=",A+B)
+print("A-B=",A-B)
+print("A*B=",A*B)
+print("A/B=",A/B)
