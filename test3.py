5274ab7b0813406b530b36e9b29ef954ca2c6b0f
Asal-Emarloo <153554023+Asal-Emarloo@users.noreply.github.com>
2023-12-13 22:00:28 +0330

    Add files via upload
    test2-3

--- test3.py
+++ test3.py
@@ -1,6 +1,15 @@
-A=int(input("Enter number= "))
-B=int(input("Enter another number="))
-print("A+B=",A+B)
-print("A-B=",A-B)
-print("A*B=",A*B)
-print("A/B=",A/B)
+import math
+Name=input("Enter your name: ")
+family=input("Enter your family: ")
+score1=float(input("Enter score1: "))
+score2=float(input("Enter score2: "))
+score3=float(input("Enter score3: "))
+avg=(score1+score2+score3)/3
+print(avg)
+if avg<=12:
+   print("failed")
+elif 12<=avg>=17:
+ print("good")
+elif avg>17: 
+ print("perfect")
+print("you are with",avg,"average")
